1c302b623e5547bf45af54de3e73394d5b8e0706
KRATOS\doug <doareis@gmail.com>
2018-12-17 00:44:10 -0200

    getter/setter no id de ClienteDTO

--- src/main/java/com/gerenciador/clientes/dto/ClienteDTO.java
+++ src/main/java/com/gerenciador/clientes/dto/ClienteDTO.java
@@ -26,6 +26,14 @@ public class ClienteDTO implements Serializable {
         this.taxa = risco.getTaxa();
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
     }
